6bf8330f6b81fec77865abacc2a4d13a951ad141
EsneiderCdD <ecd.97@hotmail.com>
2025-07-14 13:37:51 -0500

    style: refinando Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,37 +2,47 @@ import SocialIcons from './SocialIcons';
 
 export default function Footer() {
   return (
-    <footer className="bg-background-body text-text-default ">
-      <div className="container mx-auto pt-20 px-40">
-        <div  className=" mx-auto px-6 grid grid-cols-4 gap-8 ">
-          <div>
-            <h4 className="font-bold mb-4">Acerca de</h4>
-            <ul className="space-y-2">
+    <footer className="bg-background-body text-text-default">
+      <div className="max-w-[1366px] mx-auto pt-20 px-6">
+        {/* Grid principal */}
+        <div className="grid grid-cols-12 gap-8">
+          {/* Acerca de*/}
+          <div className="col-start-2 col-span-2 font-poppins ">
+            <h4 className="font-bold mb-4 text-[16px]">Acerca de</h4>
+            <ul className="space-y-4 text-[14px] ">
               <li>Programa TrainIT</li>
               <li>Contacto</li>
             </ul>
           </div>
-          <div>
-            <h4 className="font-bold mb-4">Legal</h4>
-            <ul className="space-y-2">
+
+          {/* Legal*/}
+          <div className="col-start-4 col-span-3">
+            <h4 className="font-bold mb-4 text-[16px]">Legal</h4>
+            <ul className="space-y-4 text-[14px]">
               <li>Política de cookies</li>
               <li>Política de privacidad</li>
               <li>Aviso legal y condiciones de uso</li>
             </ul>
           </div>
-          <div>
-            <h4 className="font-bold mb-4">Sedes</h4>
-            <ul className="space-y-2">
+
+          {/* Sedes*/}
+          <div className="col-start-7 col-span-3">
+            <h4 className="font-bold mb-4 text-[16px]">Sedes</h4>
+            <ul className="space-y-4 text-[14px]">
               <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
               <li>Ut enim ad minim veniam, quis nostrud exercitation</li>
             </ul>
           </div>
-          <div>
-            <h4 className="font-bold mb-4">Redes sociales</h4>
+
+          {/* Redes sociales*/}
+          <div className="col-start-10 col-span-2">
+            <h4 className="font-bold mb-4 text-[16px]">Redes sociales</h4>
             <SocialIcons />
           </div>
         </div>
-        <div className="flex justify-center mt-20 mb-20  ">
+
+        {/* Créditos */}
+        <div className="flex justify-center mt-20 mb-20 text-[14px]">
           © 2025 Copyright | Programa TrainIT
         </div>
       </div>
