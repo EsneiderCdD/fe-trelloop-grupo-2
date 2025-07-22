739ae9fa8994458825d2c5d304500854d98e0fc6
milechan <milena.concha.m@gmail.com>
2025-07-22 11:12:25 -0400

    fix:cambios en puntero en botones del navbar, y arreglo de el footer

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -26,24 +26,24 @@ const Navbar = () => {
 
             <div className="flex">
                 <div className="flex items-center justify-center mt-2 mr-[35px]">
-                    <div className="text-[16px] text-[#6a5fff] text-center w-[107px] h-[36px] mr-4" onClick={goToHome}>
+                    <button className="text-[16px] text-[#6a5fff] text-center w-[107px] h-[36px] mr-4" onClick={goToHome}>
                         Inicio
-                    </div>
-                    <div className="text-[16px] text-white text-center w-[152px] h-[36px] mr-4" onClick={goToHome}>
+                    </button>
+                    <button className="text-[16px] text-white text-center w-[152px] h-[36px] mr-4" onClick={goToHome}>
                         Acerca de
-                    </div>
-                    <div className="text-[16px] text-white text-center w-[152px] h-[36px] mr-4" onClick={goToHome}>
+                    </button>
+                    <button className="text-[16px] text-white text-center w-[152px] h-[36px] mr-4" onClick={goToHome}>
                         Contacto
-                    </div>
+                    </button>
                 </div>
 
-                <div className="h-[36px] rounded-full w-[200px] border border-[#6a5fff] text-[#6a5fff] text-center justify-center flex px-3 mt-1 mr-4 pt-1" onClick={goToLogin}>
+                <button className="h-[36px] rounded-full w-[200px] border border-[#6a5fff] text-[#6a5fff] text-center justify-center flex px-3 mt-1 mr-4 pt-1" onClick={goToLogin}>
                     LOGIN
-                </div>
+                </button>
 
-                <div className="h-[36px] rounded-full w-[200px] bg-[#6a5fff] text-white text-center justify-center flex px-3 mt-1 mr-4 pt-1" onClick={goToRegister}>
+                <button className="h-[36px] rounded-full w-[200px] bg-[#6a5fff] text-white text-center justify-center flex px-3 mt-1 mr-4 pt-1" onClick={goToRegister}>
                     REGISTRARSE
-                </div>
+                </button>
             </div>
         </div>
     );
