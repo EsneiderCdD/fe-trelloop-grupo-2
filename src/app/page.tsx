1f822a9a77f62b98aa5d45555f9b7afa8be58bdb
KevinVillca <kevtorricov@gmail.com>
2025-07-25 12:06:20 -0400

    Componente de tablero de Edit creado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
 "use client";
 
-import Header from 'components/Header/Header';
-import Footer from 'components/Footer/Footer';
-import Navbar from 'components/Navbar/Navbar';
-import Edit from 'components/Edit/Edit';
-
-
-
+import Header from "components/Header/Header";
+import Footer from "components/Footer/Footer";
+import Navbar from "components/Navbar/Navbar";
+import Edit from "components/Edit/Edit";
 
 export default function Home() {
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <main className="flex flex-col items-center justify-center min-h-screen w-full"> */}
-        {/* <Header /> */}
-      {/* </main> */}
-       <Edit/>
-      {/* <Footer /> */}
+      <Navbar />
+      <main className="flex flex-col items-center justify-center min-h-screen w-full">
+        <Header />
+      </main>
 
+      <Footer />
     </>
   );
 }
