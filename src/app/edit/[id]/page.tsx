c027ddbdf67e30fcb65fd77685bf76adc283d4ff
EsneiderCdD <ecd.97@hotmail.com>
2025-08-04 01:02:50 -0500

    fix: rename folders

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import EditBoardView from "../../../components/Edit/EditBoardView";
+import EditBoardView from "../../../components/edit/EditBoardView";
 
 export default function EditBoardPage() {
   const params = useParams();
