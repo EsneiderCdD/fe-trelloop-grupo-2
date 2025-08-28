66c366b3487629b08957cc2c2d46e3a4b18b059f
EsneiderCdD <ecd.97@hotmail.com>
2025-08-28 11:56:11 -0500

    fix: bug button closebutton

--- src/components/ui/CloseButton.tsx
+++ src/components/ui/CloseButton.tsx
@@ -1,15 +1,29 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 interface CloseButtonProps {
+  redirectTo?: string;
   onClick?: () => void;
 }
 
-const CloseButton = ({ onClick }: CloseButtonProps) => {
+const CloseButton = ({ redirectTo, onClick }: CloseButtonProps) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick(); // permite lógica personalizada si la pasas
+      return;
+    }
+    if (redirectTo) {
+      router.push(redirectTo); // redirige a la ruta que le pases
+    }
+  };
 
   return (
     <button
       className="text-white bg-[#6A5FFF] hover:bg-[#5A4FEF] transition-colors duration-200 rounded-[20px] w-[40px] h-[40px] flex justify-center items-center shadow-md"
-      onClick={onClick}
+      onClick={handleClick}
       aria-label="Cerrar vista"
     >
       <svg
@@ -20,11 +34,7 @@ const CloseButton = ({ onClick }: CloseButtonProps) => {
         stroke="currentColor"
         className="size-6"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 18 18 6M6 6l12 12"
-        />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
       </svg>
     </button>
   );
